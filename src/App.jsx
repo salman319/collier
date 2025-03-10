2be1b5e6b034377ee9f64709ceb9f05148fc7041
M Salman Saleem <msalmansaleem1998@gmail.com>
2025-03-10 17:57:14 +0500

    deploye issue

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import { useState } from 'react'
 import React from "react";
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+// import reactLogo from './assets/react.svg'
+// import viteLogo from '/vite.svg'
 import './App.css'
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home/Home";
